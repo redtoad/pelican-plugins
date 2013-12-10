59b8ff1018f53cea4a095c64af2be9ac529e9eea
Sebastian Rahlf <basti@redtoad.de>
2013-12-10 11:19:23 +0100

    Add setup.py to make plugins installable on Python path.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pelican-plugins',
+    url='http://getpelican.com/',
+    maintainer='Alexis Metaireau',
+    maintainer_email='authors@getpelican.com',
+    packages=find_packages(),
+)
+
